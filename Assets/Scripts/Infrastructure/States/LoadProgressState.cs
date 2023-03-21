1dddff95b8c2762119c5d76440bfd1e25f630dec
dancewithmoon <ihor.reshetnykov.workonly@gmail.com>
2023-03-21 23:47:07 +0200

    Base Infrastructure Setup: [+] added GameStateMachine and corresponding states (BootstrapState, LoadProgressState, LoadLevelState, GameLoopState) [+] added GameFactory and UIFactory [+] added ScreenService [+] added SaveLoadService [+] added CoroutineRunner [+] added infrastructure services

---
+++ Assets/Scripts/Infrastructure/States/LoadProgressState.cs
@@ -0,0 +1,47 @@
+﻿using Scripts.Constants;
+using Scripts.Data;
+using Scripts.Infrastructure.Services.PersistentProgress;
+using Scripts.Infrastructure.Services.SaveLoad;
+using Scripts.StaticData.Service;
+
+namespace Scripts.Infrastructure.States
+{
+    public class LoadProgressState : IState
+    {
+        private readonly IGameStateMachine _stateMachine;
+        private readonly IPersistentProgressService _progressService;
+        private readonly ISaveLoadService _saveLoadService;
+        private readonly IStaticDataService _staticDataService;
+
+        public LoadProgressState(IGameStateMachine stateMachine, IPersistentProgressService progressService,
+            ISaveLoadService saveLoadService, IStaticDataService staticDataService)
+        {
+            _stateMachine = stateMachine;
+            _progressService = progressService;
+            _saveLoadService = saveLoadService;
+            _staticDataService = staticDataService;
+        }
+
+        public void Enter()
+        {
+            LoadProgressOrInitNew();
+
+            _stateMachine.Enter<LoadLevelState, string>(Scenes.MainMenu);
+        }
+
+        public void Exit()
+        {
+        }
+
+        private void LoadProgressOrInitNew()
+        {
+            _progressService.Progress = _saveLoadService.LoadProgress() ?? InitNewProgress();
+        }
+
+        private PlayerProgress InitNewProgress()
+        {
+            PlayerProgress progress = new PlayerProgress();
+            return progress;
+        }
+    }
+}
