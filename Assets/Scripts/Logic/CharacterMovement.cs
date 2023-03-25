537e17fd6f591859a79025414a4c8cb7e1bcb057
dancewithmoon <ihor.reshetnykov.workonly@gmail.com>
2023-03-25 19:54:59 +0200

    Updated Movement script: [+] added gravity vector [*] Improved code readability

--- Assets/Scripts/Logic/CharacterMovement.cs
+++ Assets/Scripts/Logic/CharacterMovement.cs
@@ -8,14 +8,12 @@ namespace Scripts.Logic
         [SerializeField] private float _speed;
         private CharacterController _characterController;
 
-        private void Awake()
-        {
+        private void Awake() => 
             _characterController = GetComponent<CharacterController>();
-        }
 
-        private void Update()
-        {
-            _characterController.Move(Vector3.right * (_speed * Time.deltaTime));
-        }
+        private void Update() => 
+            _characterController.Move(Physics.gravity + MovementVector);
+
+        private Vector3 MovementVector => Vector3.right * (_speed * Time.deltaTime);
     }
 }
