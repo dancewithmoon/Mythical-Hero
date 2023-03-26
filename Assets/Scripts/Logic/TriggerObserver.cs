636bd3c6a1629a74edef3a4e99d53ffa23590797
dancewithmoon <ihor.reshetnykov.workonly@gmail.com>
2023-03-26 22:21:02 +0300

    Added Trigger Observer: [+] added Trigger Observer script [+] attached Trigger Observer with Sphere Collider to the Hero prefab

---
+++ Assets/Scripts/Logic/TriggerObserver.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Scripts.Utils;
+using UnityEngine;
+
+namespace Scripts.Logic
+{
+    [RequireComponent(typeof(Collider))]
+    public class TriggerObserver : MonoBehaviour
+    {
+        private readonly List<GameObject> _triggeredObjects = new List<GameObject>();
+        
+        [SerializeField] private LayerMask _layerMask;
+
+        public IReadOnlyList<GameObject> TriggeredObjects => _triggeredObjects;
+        
+        public event Action<Collider> TriggerEnter;
+        public event Action<Collider> TriggerExit;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.HasLayer(_layerMask) == false) 
+                return;
+            
+            if(_triggeredObjects.Contains(other.gameObject))
+                return;
+                
+            _triggeredObjects.Add(other.gameObject);
+            TriggerEnter?.Invoke(other);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.HasLayer(_layerMask) == false) 
+                return;
+            
+            if(_triggeredObjects.Contains(other.gameObject) == false)
+                return;
+
+            _triggeredObjects.Remove(other.gameObject);
+            TriggerExit?.Invoke(other);
+        }
+    }
+}
