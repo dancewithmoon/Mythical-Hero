4ea4fa716470103239d0b798bc9689335f67fa93
dancewithmoon <ihor.reshetnykov.workonly@gmail.com>
2023-03-29 01:01:20 +0300

    Updated HeroAttack
    [+] added subscription on TargetExit event to stop attack when target exit the attack zone
    [+] added subscription on Attack animation event to attack target when attack event is handled

--- Assets/Scripts/Hero/HeroAttack.cs
+++ Assets/Scripts/Hero/HeroAttack.cs
@@ -1,24 +1,62 @@
 ﻿using Scripts.Logic;
+using Scripts.Utils;
 using UnityEngine;
 
 namespace Scripts.Hero
 {
-    [RequireComponent(typeof(HeroAnimator))]
+    [RequireComponent(typeof(CharacterMovement), typeof(CharacterDamage))]
+    [RequireComponent(typeof(HeroAnimator), typeof(HeroAnimationEventHandler))]
     public class HeroAttack : MonoBehaviour
     {
         [SerializeField] private TriggerObserver _trigger;
+        private CharacterMovement _heroMovement;
+        private CharacterDamage _heroDamage;
         private HeroAnimator _heroAnimator;
-        
-        private void Awake() => 
+        private HeroAnimationEventHandler _animationEventHandler;
+
+        private void Awake()
+        {
+            _heroMovement = GetComponent<CharacterMovement>();
+            _heroDamage = GetComponent<CharacterDamage>();
             _heroAnimator = GetComponent<HeroAnimator>();
+            _animationEventHandler = GetComponent<HeroAnimationEventHandler>();
+        }
 
-        private void OnEnable() => 
+        private void OnEnable()
+        {
             _trigger.TriggerEnter += OnTargetEnteredAttackZone;
+            _trigger.TriggerExit += OnTargetExitAttackZone;
+            _animationEventHandler.Attacked += OnAttackHandled;
+        }
 
-        private void OnDisable() => 
+        private void OnDisable()
+        {
             _trigger.TriggerEnter -= OnTargetEnteredAttackZone;
+            _animationEventHandler.Attacked -= OnAttackHandled;
+        }
 
-        private void OnTargetEnteredAttackZone(Collider target) => 
+        private void OnTargetEnteredAttackZone(Collider target)
+        {
+            _heroMovement.enabled = false;
             _heroAnimator.SetAttackValue(true);
+        }
+
+        private void OnTargetExitAttackZone(Collider target)
+        {
+            _heroAnimator.SetAttackValue(false);
+            this.Invoke(StopAttack, 0.5f);
+        }
+
+        private void StopAttack() => 
+            _heroMovement.enabled = true;
+
+        private void OnAttackHandled()
+        {
+            foreach (GameObject target in _trigger.TriggeredObjects)
+            {
+                target.GetComponent<IDamageable>().ApplyDamage();
+                _heroDamage.ApplyDamage();
+            }
+        }
     }
 }
