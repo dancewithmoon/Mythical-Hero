9ccf0f5a9240bc6228ebb36ba1a2340621d16407
dancewithmoon <ihor.reshetnykov.workonly@gmail.com>
2023-03-30 01:17:35 +0300

    Added Lich Animator
    [+] added LichAnimator
    [+] attached LichAnimator to Lich prefab

---
+++ Assets/Scripts/Logic/Enemy/Animations/LichAnimator.cs
@@ -0,0 +1,23 @@
+﻿using Scripts.Logic.Animations;
+using UnityEngine;
+
+namespace Scripts.Logic.Enemy.Animations
+{
+    [RequireComponent(typeof(Animator))]
+    public class LichAnimator : CharacterAnimator
+    {
+        private static readonly int AttackParameterHash = Animator.StringToHash("Attack");
+        private static readonly int StunParameterHash = Animator.StringToHash("ToStun");
+        
+        private Animator _animator;
+
+        private void Awake() => 
+            _animator = GetComponent<Animator>();
+
+        public override void SetAttackValue(bool value) => 
+            _animator.SetBool(AttackParameterHash, value);
+
+        public override void SetStunTrigger() => 
+            _animator.SetTrigger(StunParameterHash);
+    }
+}
