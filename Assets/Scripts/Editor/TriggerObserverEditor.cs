134f373d403719d06898e62e9ba1d269ace56809
dancewithmoon <ihor.reshetnykov.workonly@gmail.com>
2023-04-23 13:48:51 +0300

    Added Trigger Observer Editor script
    [+] added TriggerObserverEditor to display the list of triggered objects

---
+++ Assets/Scripts/Editor/TriggerObserverEditor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Scripts.Logic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(TriggerObserver))]
+    public class TriggerObserverEditor : UnityEditor.Editor
+    {
+        private TriggerObserver _triggerObserver;
+
+        private void OnEnable()
+        {
+            _triggerObserver = (TriggerObserver)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            
+            FieldInfo fieldInfo = typeof(TriggerObserver).GetField("_triggeredObjects", BindingFlags.Instance | BindingFlags.NonPublic);
+            List<GameObject> triggeredObjects = (List<GameObject>)fieldInfo.GetValue(_triggerObserver);
+
+            if (triggeredObjects == null) 
+                return;
+            
+            GUILayout.Space(10f);
+            GUILayout.Label("Triggered Objects:");
+            foreach (GameObject obj in triggeredObjects)
+            {
+                EditorGUILayout.ObjectField(obj, typeof(GameObject), true);
+            }
+        }
+    }
+}
