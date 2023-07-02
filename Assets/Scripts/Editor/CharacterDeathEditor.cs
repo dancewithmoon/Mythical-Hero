34c167729c4b9dd82e5256a755f18beca17765c3
dancewithmoon <ihor.reshetnykov.workonly@gmail.com>
2023-07-03 01:23:12 +0300

    Added CharacterDeath editor script

---
+++ Assets/Scripts/Editor/CharacterDeathEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Scripts.Logic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(CharacterDeath))]
+    public class CharacterDeathEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            if (GUILayout.Button("Die"))
+            {
+                MethodInfo dieMethod = typeof(CharacterDeath).GetMethod("Die", BindingFlags.Instance | BindingFlags.NonPublic);
+                dieMethod.Invoke(target, null);
+            }
+        }
+    }
+}
